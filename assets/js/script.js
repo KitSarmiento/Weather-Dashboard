00b5b914bcf00220ceb4cc742289d4591a359ec7
KitSarmiento <ksarmiento020@gmail.com>
2023-09-29 13:05:55 -0400

    Added an event listener function for the search button.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,21 @@
 var apiKey = "9484fb3037fb24e863ff617d2c22b3b1";
+var cityInput = document.querySelector(".cityName");
+var searchBtn = document.querySelector(".searchBtn");
+var currentData = document.querySelector(".currentData");
+var daysForecast = document.querySelector(".daysForecast");
+var weatherDashboard = document.getElementById("weatherDashboard");
+
+// Event listener for the search button
+searchBtn.addEventListener("click", function () {
+  var city = cityInput.value.trim();
+
+  if (city) {
+    WeatherData(city);
+    updateSearchHistory(city);
+    cityInput.value = "";
+    toggleWeatherDashboard(true);
+  }
+});
 
 // function to toggle the dashboard
 function toggleWeatherDashboard() {
