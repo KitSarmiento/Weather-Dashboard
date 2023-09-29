7b50ed66744dd3e86fc8472faca501a627c5e077
KitSarmiento <ksarmiento020@gmail.com>
2023-09-29 13:08:32 -0400

    Fetched the weather data from the OpenWeatherMap.

--- assets/js/script.js
+++ assets/js/script.js
@@ -26,3 +26,36 @@ function toggleWeatherDashboard() {
     weatherDashboard.style.display = "none";
   }
 }
+
+// Function to fetch weather data from the OpenWeatherMap
+function WeatherData(cityName) {
+  var currentWeatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`;
+  var forecastUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${apiKey}`;
+
+  // Used the module activity as a reference for this code.
+  // Fetch the current forecast
+  fetch(currentWeatherUrl)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      displayCurrentWeather(data);
+    })
+    .catch(function (error) {
+      console.error("Error fetching current weather:", error);
+      displayErrorMessage();
+    });
+
+  // Fetch 5-days forecast
+  fetch(forecastUrl)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      displayForecast(data);
+    })
+    .catch(function (error) {
+      console.error("Error fetching forecast data:", error);
+      displayErrorMessage();
+    });
+}
