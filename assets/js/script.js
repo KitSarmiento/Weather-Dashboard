705d9f0fa16d4fbce84752f4d2f65040e8163453
KitSarmiento <ksarmiento020@gmail.com>
2023-09-29 13:17:33 -0400

    Added the function to display the weather and serach history to add to local storage.

--- assets/js/script.js
+++ assets/js/script.js
@@ -59,3 +59,79 @@ function WeatherData(cityName) {
       displayErrorMessage();
     });
 }
+
+// Function to display current weather data
+function displayCurrentWeather(data) {
+  var weatherIcon = data.weather[0].icon;
+  var temperatureKelvin = data.main.temp; // convert the Kelvin to Farenheit.
+  var temperatureFahrenheit = (
+    ((temperatureKelvin - 273.15) * 9) / 5 +
+    32
+  ).toFixed(2);
+
+  currentData.innerHTML = `
+        <h2>${data.name} (${dayjs().format("MM/DD/YYYY")})</h2>
+        <img src="https://openweathermap.org/img/wn/${weatherIcon}.png" alt="Weather Icon">
+        <p class="titleDetails">Temperature: ${temperatureFahrenheit}°F</p>
+        <p class="titleDetails">Wind: ${data.wind.speed} m/s</p>
+        <p class="titleDetails">Humidity: ${data.main.humidity}%</p>
+      `;
+}
+
+// Function to 5 days forecast.
+function displayForecast(data) {
+  var forecastList = data.list;
+  var displayedDates = []; // Used an array for the dates.
+  forecastList.forEach((item, index) => {
+    var weatherIcon = item.weather[0].icon;
+    var date = dayjs(item.dt_txt).format("MM/DD/YYYY");
+    var temperatureKelvin = item.main.temp;
+    var temperatureFahrenheit = (
+      ((temperatureKelvin - 273.15) * 9) / 5 +
+      32
+    ).toFixed(2);
+
+    if (!displayedDates.includes(date)) {
+      displayedDates.push(date);
+      var forecastElement = document.querySelector(
+        `.cardsDetails:nth-child(${displayedDates.length})`
+      );
+
+      if (forecastElement) {
+        forecastElement.innerHTML = `
+            <h2>${date}</h2>
+            <img src="https://openweathermap.org/img/wn/${weatherIcon}.png" alt="Weather Icon">
+            <p class="titleDetails">Temperature: ${temperatureFahrenheit}°F</p>
+            <p class="titleDetails">Wind: ${item.wind.speed} m/s</p>
+            <p class="titleDetails">Humidity: ${item.main.humidity}%</p>
+          `;
+      }
+    }
+  });
+}
+// Error message
+function displayErrorMessage() {
+  currentData.innerHTML = `<p class="error">Something went wrong. Please try again.</p>`;
+  daysForecast.innerHTML = "";
+}
+
+// Event listener for the search history
+function updateSearchHistory(city) {
+  var searchHistory = document.querySelector(".searchHistory");
+  var listItem = document.createElement("li");
+  listItem.textContent = city;
+  listItem.addEventListener("click", () => {
+    WeatherData(city);
+    toggleWeatherDashboard(true);
+  });
+  searchHistory.appendChild(listItem);
+}
+
+// Function to add the cities to the local storage.
+function SearchStorage(search) {
+  var searches = JSON.parse(localStorage.getItem("searches")) || [];
+  searches.push(search);
+  localStorage.setItem("searches", JSON.stringify(searches));
+}
+var cityName = `${cityName}`;
+SearchStorage(cityName);
